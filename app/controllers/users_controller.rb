b7dcdd30e1bdabb712c003459561d33c14dad609
makikurokawa <110467242+makikurokawa@users.noreply.github.com>
2022-09-15 13:54:44 +0000

    [途中]09152254

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  get 'users/edit'
-  get 'users' => 'users#index'
-  get 'users/:id' => 'user#show', as: 'user'
-
-  resources :books, only: [:index, :show, :edit]
+  resources :users, only:[:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :create]
 
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
