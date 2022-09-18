c453c31f9be1fcfdd6a91299116c9e580cddd123
makikurokawa <110467242+makikurokawa@users.noreply.github.com>
2022-09-18 08:06:17 +0000

    [途中]091817:06

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
@@ -10,6 +12,24 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
+    else
+      redirect_to user_path(@user.id)
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(resource), notice: 'Welcome! You have signed up successfully.'
+    else
+      redirect_to root_path
+    end
   end
 
   private
